1b6ee5908c310c6f7164754ed77121cb851a49d1
Florian <florianpasquett@gmail.com>
2022-05-05 10:40:12 +0200

    Ajout Formation

--- js/script.js
+++ js/script.js
@@ -5,16 +5,12 @@ function carouselTo(slide) {
 
 //Navbar buttons
 let nav_button = document.getElementsByClassName("nav-link");
-//carousel buttons Prev/Next
-let prev_btn = document.querySelector(".carousel-control-prev");
-let next_btn = document.querySelector(".carousel-control-next");
+
 //carousel
 let carouselElement = document.getElementById("carousel1");
 let carousel =  new bootstrap.Carousel(carouselElement, {
     interval: false
 });
-
-
 
 //addEventListener for buttons in navBar
 for(let i = 0; i < nav_button.length; i++){
@@ -23,11 +19,3 @@ for(let i = 0; i < nav_button.length; i++){
     }, {passive: true});
 }
 
-//addEventListener for Prev/Next in carousel
-prev_btn.addEventListener("click", () => {
-    carousel.prev();
-}, {passive: true});
-
-next_btn.addEventListener("click", () => {
-    carousel.next();
-}, {passive: true});
